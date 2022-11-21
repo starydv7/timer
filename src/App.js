72153eb2662f387ebbf97ef7579980e2ba59f0f8
starydv7 <pawanstar72926@gmail.com>
2022-11-21 13:36:35 +0530

    google timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import GoogleTimer from './components/GoogleTimer';
 import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
-     <Timer/>
+      <Timer />
+      <GoogleTimer/>
     </div>
   );
 }
