3d76a1677b572bb817783186cf49893063b168fc
starydv7 <pawanstar72926@gmail.com>
2022-11-21 13:35:04 +0530

    timer

---
+++ src/components/GoogleTimer.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Styles from "./Time.module.css";
+
+const GoogleTimer = () => {
+  const [micro, setMicro] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [hour, setHour] = useState(0);
+
+  useEffect(() => {
+    var timer = setInterval(() => {
+      setMicro(micro + 1);
+      if (micro === 99) {
+        setSecond(second + 1);
+        setMicro(0);
+      } else if (second === 59) {
+        setMinute(minute + 1);
+        setSecond(0);
+      } else if (minute === 59) {
+        setHour(hour + 1);
+        setMinute(0);
+      }
+    }, 10);
+    return () => clearInterval(timer);
+  });
+  return (
+    <div className={Styles.time}>
+      <h2 style={{ color: "green" }}>
+        Hour:{hour}:Minute:{minute}:Seconds:{second}:{micro}
+      </h2>
+    </div>
+  );
+};
+export default GoogleTimer;
