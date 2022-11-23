d81571c987324dd97f9730ae2bf981e8ebcf3b73
starydv7 <pawanstar72926@gmail.com>
2022-11-23 22:54:09 +0530

    timer

--- src/components/Timer3.jsx
+++ src/components/Timer3.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 
-const timer3 = () => {
+const Timer3 = () => {
     const [data, setData] = useState("");
   return (
     <div>timer3</div>
   )
 }
 
-export default timer3
+export default Timer3;
